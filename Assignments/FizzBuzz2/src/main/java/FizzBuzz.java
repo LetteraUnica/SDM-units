5df6f78a4d5f5d7246425cb633769936aefbce15
LetteraUnica <lorenzocav97@gmail.com>
2021-12-09 20:30:11 +0100

    Green Fizz and Buzz

---
+++ Assignments/FizzBuzz2/src/main/java/FizzBuzz.java
@@ -0,0 +1,12 @@
+public class FizzBuzz {
+
+    public String convert(int i) {
+        if (i%3 == 0) {
+            return "Fizz";
+        }
+        if (i%5 == 0) {
+            return "Buzz";
+        }
+        return String.valueOf(i);
+    }
+}
